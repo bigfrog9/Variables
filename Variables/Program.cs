cadae5532e6b94a5f1e143e1eb8f9f585070f775
John Wohler <johndoubleyou11@gmail.com>
2022-09-20 08:56:50 -0300

    Initial commit

---
+++ Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is John's project for Variables, I'll be uploading this on Github.");
+            Console.ReadKey(true);
+        }
+    }
+}
